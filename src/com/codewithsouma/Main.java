ae0a5ff2d3614c72dd12a351eb1b87a637deab19
Souma <souma.hitech@gmail.com>
2020-11-12 20:11:22 +0530

    Procedural vs oops

--- src/com/codewithsouma/Main.java
+++ src/com/codewithsouma/Main.java
@@ -3,13 +3,20 @@ package com.codewithsouma;
 public class Main {
 
     public static void main(String[] args) {
+        // OOP Style
+        var employee = new Employee();
+        employee.baseSalary = 50_000;
+        employee.hourlyRate = 20;
+        int wageOfEmployee = employee.calculateWage(10);
+        System.out.println(wageOfEmployee);
+
+        //Procedural Style
         int baseSalary = 50_000;
         int extraHours = 10;
         int hourlyRate = 20;
 
         int wage = calculateWage(baseSalary,extraHours,hourlyRate);
         System.out.println(wage);
-
     }
 
     public static int calculateWage(
