582507d603170d60045a9168b707f161addcc409
Souma <souma.hitech@gmail.com>
2020-11-12 20:30:20 +0530

    add getter and setter in Employee class

--- src/com/codewithsouma/Employee.java
+++ src/com/codewithsouma/Employee.java
@@ -1,8 +1,28 @@
 package com.codewithsouma;
 
 public class Employee {
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
+
+    public void setBaseSalary(int baseSalary) {
+        if(baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or less.");
+        this.baseSalary = baseSalary;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if(hourlyRate <= 0)
+            throw new IllegalArgumentException("Hourly rate cannot be 0 or negative.");
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
 
     public int calculateWage(int extraHours){
         return baseSalary + (extraHours * hourlyRate);
