1f3ce16e2941bc8e91c6650b0db54dae54a211f5
Souma <souma.hitech@gmail.com>
2020-11-14 22:10:58 +0530

    add constructor in textbox class and call super class constructor

--- src/com/codewithsouma/Main.java
+++ src/com/codewithsouma/Main.java
@@ -3,16 +3,7 @@ package com.codewithsouma;
 public class Main {
 
     public static void main(String[] args) {
-//        var employee1 = new Employee(48_000);
-//        var employee2 = new Employee(50_000, 20);
-//        int wageOfEmployee1 = employee1.calculateWage();
-//        int wageOfEmployee2 = employee2.calculateWage(10);
-//        System.out.println("Wage of employee1: "+wageOfEmployee1);
-//        System.out.println("Wage of employee2: "+wageOfEmployee2);
-//        Employee.printNumberOfEmployees();
-        var control = new TextBox();
-        control.disable();
-        System.out.println(control.isEnabled());
+        var box1 = new TextBox();
 
     }
 
--- src/com/codewithsouma/TextBox.java
+++ src/com/codewithsouma/TextBox.java
@@ -3,6 +3,11 @@ package com.codewithsouma;
 public class TextBox extends UIControl{
     public String text = "";
 
+    public TextBox() {
+        super(true);
+        System.out.println("TextBox");
+    }
+
     public void setText(String text){
         this.text = text;
     }
