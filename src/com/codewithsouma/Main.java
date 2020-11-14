cc362c3f24e3ed2e1a80559f62e62bb2f22be3ab
Souma <souma.hitech@gmail.com>
2020-11-14 22:40:27 +0530

    Complete upcasting and downcasting

--- src/com/codewithsouma/Main.java
+++ src/com/codewithsouma/Main.java
@@ -3,10 +3,18 @@ package com.codewithsouma;
 public class Main {
 
     public static void main(String[] args) {
-        var box1 = new TextBox();
-        box1.text = "Hello World!!";
-        System.out.println(box1);
+       var control = new UIControl(true);
+        var textBox = new TextBox();
+        show(textBox);// textBox is a control because it is a child of UIControl
+        show(control);//not an textBox
 
+    }
+    public static void show(UIControl control){// upcasting
+        if (control instanceof TextBox){
+            var textBox = (TextBox)control;//down casting
+            textBox.text = "Hello World!!!";
+        }
+        System.out.println(control);
     }
 
 }
