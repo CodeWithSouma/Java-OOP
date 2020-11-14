47848c07b11b078015df645502a2a27b051efb37
Souma <souma.hitech@gmail.com>
2020-11-14 22:24:22 +0530

    Override toString in TextBox class

--- src/com/codewithsouma/Main.java
+++ src/com/codewithsouma/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         var box1 = new TextBox();
+        box1.text = "Hello World!!";
+        System.out.println(box1);
 
     }
 
