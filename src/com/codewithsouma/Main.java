2817caaad38f4cc76c8f5d40c7387e1fcb7ef37c
Souma <souma.hitech@gmail.com>
2020-11-14 23:20:37 +0530

    Override equals methods in point class

--- src/com/codewithsouma/Main.java
+++ src/com/codewithsouma/Main.java
@@ -3,18 +3,13 @@ package com.codewithsouma;
 public class Main {
 
     public static void main(String[] args) {
-       var control = new UIControl(true);
-        var textBox = new TextBox();
-        show(textBox);// textBox is a control because it is a child of UIControl
-        show(control);//not an textBox
+        var point1 = new Point(1, 2);
+        var point2 = new Point(1,2);
+
+        System.out.println(point1 == point2);
+        System.out.println(point1.equals(point2));
+        System.out.println(point1.hashCode());
+        System.out.println(point2.hashCode());
 
     }
-    public static void show(UIControl control){// upcasting
-        if (control instanceof TextBox){
-            var textBox = (TextBox)control;//down casting
-            textBox.text = "Hello World!!!";
-        }
-        System.out.println(control);
-    }
-
 }
