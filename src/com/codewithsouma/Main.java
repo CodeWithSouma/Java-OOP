c7372230d661d3fb21d68eb5205c033a29203e09
Souma <souma.hitech@gmail.com>
2020-11-15 13:05:41 +0530

    Complete setter Injection

--- src/com/codewithsouma/Main.java
+++ src/com/codewithsouma/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
        var calculator = new TaxCalculator2018(100_000);
        var report = new TaxReport(calculator);//Inject calculator class
+        report.show();
+
+        report.setCalculator(new TaxCalculator2019());//setter injection
+        report.show();
     }
 }
