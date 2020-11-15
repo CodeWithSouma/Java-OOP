51d7b77b611c45d137e825fb2690a3427677418d
Souma <souma.hitech@gmail.com>
2020-11-15 13:14:21 +0530

    Complete method injection

--- src/com/codewithsouma/Main.java
+++ src/com/codewithsouma/Main.java
@@ -3,11 +3,13 @@ package com.codewithsouma;
 public class Main {
 
     public static void main(String[] args) {
-       var calculator = new TaxCalculator2018(100_000);
-       var report = new TaxReport(calculator);//Inject calculator class
-        report.show();
+       var calculator2018 = new TaxCalculator2018(100_000);
+//       var report = new TaxReport(calculator2018);//Inject calculator class
+        var report = new TaxReport();
+        report.show(calculator2018);//method injection
 
-        report.setCalculator(new TaxCalculator2019());//setter injection
-        report.show();
+//        report.setCalculator(new TaxCalculator2019());//setter injection
+        var calculator2019 = new TaxCalculator2019();
+        report.show(calculator2019);//method injection
     }
 }
--- src/com/codewithsouma/TaxReport.java
+++ src/com/codewithsouma/TaxReport.java
@@ -4,17 +4,18 @@ public class TaxReport {
     private TaxCalculator calculator;
 
     //Constructor Injection
-    public TaxReport(TaxCalculator calculator) {
-        this.calculator = calculator;
-    }
+//    public TaxReport(TaxCalculator calculator) {
+//        this.calculator = calculator;
+//    }
 
-    public void show(){
+    //method injection
+    public void show(TaxCalculator calculator){
         var tax = calculator.calculateTax();
         System.out.println(tax);
     }
 
     //Setter Injection
-    public void setCalculator(TaxCalculator calculator) {
-        this.calculator = calculator;
-    }
+//    public void setCalculator(TaxCalculator calculator) {
+//        this.calculator = calculator;
+//    }
 }
